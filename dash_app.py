5c248f545e8348b235c3da7b9435727dcd4aea19
Victor-CHC <gekidanvictor@gmail.com>
2020-08-25 00:55:43 +0100

    plot graph from user input

--- dash_app.py
+++ dash_app.py
@@ -2,27 +2,78 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas_datareader.data as web
+import datetime
+
+#start = datetime.datetime(2018, 1, 1)
+#end = datetime.datetime.now()
+
+#stock = 'TSLA'
+
+#df = web.DataReader(stock, 'yahoo', start, end)
 
 app = dash.Dash()
 
-'''
-app.layout = html.Div(children = [
-    html.H1('Dash tutorials'),
-    dcc.Graph(id='example',
+app.layout = html.Div(children=[
+    
+    html.Div(children='''
+             symbol to graph:
+             '''),
+    
+    dcc.Input(id='input', value='', type='text'),
+    html.Div(id='output-graph')
+    ])
+
+@app.callback(
+    Output(component_id='output-graph', component_property='children'),
+    [Input(component_id='input', component_property='value')]
+        )
+def update_graph(input_data):
+    ''' 
+
+    Parameters
+    ----------
+    input_data : Str
+        Name of share or commodity
+
+    Returns
+    -------
+    Graph
+        Graph of closing price between Jan 1 2018 and now.
+
+    '''
+    start = datetime.datetime(2018, 1, 1)
+    end = datetime.datetime.now()
+    df = web.DataReader(input_data, 'yahoo', start, end)
+    
+    return dcc.Graph(id='example-graph',
               figure = {
-                  'data':[{'x':[1,2,3,4,5], 'y':[5,4,2,6,1], 
-                           'type':'line',
-                           'name':'boats'},
-                          {'x':[1,2,3,4,5], 'y':[1,7,2,7,8], 
-                           'type':'bar',
-                           'name':'cars'},
+                  'data':[
+                      {'x':df.index, 'y':df.Close, 'type':'line','name':input_data},
                           ],
                   'layout': {
-                      'title':'Basic Dash Example'
+                      'title':'stock'
                       }
-                  })
+                  }
+              )
+
+             
+'''
+app.layout = html.Div(children = [
+    html.H1('OMG Finance'),
+    dcc.Graph(id='example-graph',
+              figure = {
+                  'data':[
+                      {'x':df.index, 'y':df.Close, 'type':'line','name':stock},
+                          ],
+                  'layout': {
+                      'title':'stock'
+                      }
+                  }
+              )
     
     ])
+'''
 '''
 
 app.layout = html.Div(children=[
@@ -38,6 +89,6 @@ def update_value(input_data):
         return str(float(input_data)**2)
     except:
         return "Some error"
-
+'''
 if __name__ == '__main__':
     app.run_server(debug=True)
